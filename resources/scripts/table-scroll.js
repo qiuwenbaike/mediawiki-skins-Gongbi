ee65b0af99666808b8c2603532fd044f8aac1dea
WaitSpring <me@waitspring.com>
2025-11-09 09:22:56 +0800

    replace license headers with SPDX ID

--- resources/scripts/table-scroll.js
+++ resources/scripts/table-scroll.js
@@ -1,21 +1,9 @@
 /**
  * Gongbi Skin
  * © 2015-2021 Issara
- * © 2022-2024 WaitSpring
+ * © 2022-2025 WaitSpring
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * @license GPL-2.0-or-later
  */
 
 'use strict';
@@ -131,7 +119,6 @@ mw.hook( 'wikipage.content' ).add( ( $content ) => {
 		const $mirror = $( this ).siblings( '.content-table' ).first();
 
 		// Only do this here if we're not already mirroring the table
-		// eslint-disable-next-line no-jquery/no-class-state
 		if ( !$( this ).hasClass( 'inactive' ) ) {
 			$mirror.scrollLeft( $( this ).scrollLeft() );
 		}
