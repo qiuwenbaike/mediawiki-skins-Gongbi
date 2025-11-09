fb21073fdb21dd66fae053246f8fce7dec7971c6
WaitSpring <me@waitspring.com>
2025-11-09 09:22:35 +0800

    add eslint compat modules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
+import { fixupConfigRules } from "@eslint/compat";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -24,29 +25,22 @@ export default [
 			'**/package.json',
 			'**/package-lock.json',
 			'**/pnpm-lock.yaml',
-			'resources/libraries/',
 			'Gruntfile.js',
 		]
 	},
-	...compat.extends(
-		"wikimedia/jquery",
-		'wikimedia/language/es2022',
-		'wikimedia/mediawiki'
+	...fixupConfigRules(
+		...compat.extends(
+			'wikimedia/client',
+			'wikimedia/language/es2022',
+			'wikimedia/mediawiki'
+		),
 	),
 	{
 		languageOptions: {
 			globals: {
 				...globals.browser,
 				exports: true
-			},
-
-			ecmaVersion: 11,
-			sourceType: 'module'
+			}
 		},
-
-		rules: {
-			'space-in-parens': 0,
-			'no-jquery/no-global-selector': 'off'
-		}
 	}
 ];
